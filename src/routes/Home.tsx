a5534af2483af0e97c224c21b8354a8609051101
Jindrich Jehlicka <jindrich.jehlicka@mibcon.cz>
2021-11-04 11:50:09 +0100

    add a subtitle to homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,6 +4,7 @@ import Page from "../components/Page";
 const Home: React.FC = () => {
     return <Page>
         <h1>Welcome to Homepage</h1>
+        <h2>Please, log in to access your dashboard</h2>
     </Page>;
 };
 
